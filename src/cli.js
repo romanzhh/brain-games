4e923a4c8b9b621b0d7dedd4ebd7c12bab62e83a
roman <storkanderon222@gmail.com>
2022-09-22 19:23:16 +0300

    game

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function () {
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
-};
-
+export default function askName() {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+}
